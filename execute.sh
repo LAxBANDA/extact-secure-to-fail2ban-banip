c258ce749de8124c097c0436312e69519955c9df
LAxBANDA <alekos@bandaservers.cl>
2024-12-27 14:57:38 -0300

    first commit

--- execute.sh
+++ execute.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# Nombre de la sesión de screen (puedes cambiarlo si lo deseas)
-SCREEN_SESSION_NAME="fail2ban-session"
-
 # Ruta del archivo de Node.js que procesa los logs
 NODE_SCRIPT="./procesar_logs.js"
 
 SSH_IP=$(echo $SSH_CLIENT | awk '{ print $1 }')
-
-TMUX_SESSION_NAME="fail2ban-session"
-
 
 # Verificar si el archivo de Node.js existe
 if [ ! -f "$NODE_SCRIPT" ]; then
@@ -17,15 +11,10 @@ if [ ! -f "$NODE_SCRIPT" ]; then
     exit 1
 fi
 
-# Verificar si la sesión de tmux ya existe
-if tmux has-session -t "$TMUX_SESSION_NAME" 2>/dev/null; then
-    echo "La sesión de tmux '$TMUX_SESSION_NAME' ya está en ejecución."
-else
-    echo "Iniciando una nueva sesión de tmux..."
-    # Crear una nueva sesión de tmux y ejecutar el script de Node.js con la IP SSH y la IP a excluir
-    tmux new-session -d -s "$TMUX_SESSION_NAME" "node $NODE_SCRIPT $SSH_IP"
-    echo "El script de Node.js se está ejecutando en la sesión de tmux '$TMUX_SESSION_NAME'."
-fi
+node $NODE_SCRIPT $SSH_IP
+
+# Crear una nueva sesión de tmux y ejecutar el script de Node.js con la IP SSH y la IP a excluir
+echo "El script de Node.js se está ejecutando"
 
 # Esperar un poco para asegurarse de que el script de Node.js termine su ejecución
 echo "Esperando que el script de Node.js termine..."
